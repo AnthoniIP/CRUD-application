8a6d28328a45077bcfde16f2670b43d4ef982a34
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2021-10-23 14:35:19 -0300

    First Commit

---
+++ app/src/main/java/com/ipsoft/crudapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ipsoft.crudapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ipsoft.crudapplication.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
